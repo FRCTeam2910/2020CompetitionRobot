e3380d8db18acff6971fcfddd18345049f6bebe7
Vlad <vladimirkupryukhin@gmail.com>
2020-01-18 11:25:50 -0800

    Setup wheel subsystem to command schedular

--- src/main/java/org/frcteam2910/c2020/Robot.java
+++ src/main/java/org/frcteam2910/c2020/Robot.java
@@ -1,7 +1,5 @@
 package org.frcteam2910.c2020;
 
-import com.revrobotics.ColorSensorV3;
-import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.frcteam2910.common.robot.UpdateManager;
@@ -9,7 +7,8 @@ import org.frcteam2910.common.robot.UpdateManager;
 public class Robot extends TimedRobot {
     private RobotContainer robotContainer = new RobotContainer();
     private UpdateManager updateManager = new UpdateManager(
-        robotContainer.getDrivetrainSubsystem()
+            robotContainer.getDrivetrainSubsystem(),
+            robotContainer.getWheelOfFortuneSubsystem()
     );
 
     @Override
