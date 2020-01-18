9061e290d78e6b8a59a3015965438ed90b5de46e
Vlad <vladimirkupryukhin@gmail.com>
2020-01-18 10:53:33 -0800

    Adds the color wheel subsystem

--- src/main/java/org/frcteam2910/c2020/Robot.java
+++ src/main/java/org/frcteam2910/c2020/Robot.java
@@ -1,5 +1,7 @@
 package org.frcteam2910.c2020;
 
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.frcteam2910.common.robot.UpdateManager;
