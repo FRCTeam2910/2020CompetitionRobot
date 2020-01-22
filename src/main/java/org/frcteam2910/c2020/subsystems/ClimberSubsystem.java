7aace5414aeda6d17db8699f944d433c910235a8
Tsumiki <cppacaba@gmail.com>
2020-01-21 18:58:24 -0800

    Initial Commit

---
+++ src/main/java/org/frcteam2910/c2020/subsystems/ClimberSubsystem.java
@@ -0,0 +1,14 @@
+package org.frcteam2910.c2020.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import org.frcteam2910.common.robot.UpdateManager;
+
+public class ClimberSubsystem implements Subsystem, UpdateManager.Updatable {
+
+    @Override
+    public void update(double time, double dt){
+
+    }
+
+}
