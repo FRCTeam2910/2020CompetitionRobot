85605e6befe75340a03de9d1a28b64eea37d5d55
Charles Immendorf <ichazi@yahoo.com>
2020-01-22 20:24:03 -0800

    Added PID constants to wheel of fortune spinner

--- src/main/java/org/frcteam2910/c2020/subsystems/WheelOfFortuneSubsystem.java
+++ src/main/java/org/frcteam2910/c2020/subsystems/WheelOfFortuneSubsystem.java
@@ -1,8 +1,10 @@
 package org.frcteam2910.c2020.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.revrobotics.ColorSensorV3;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -20,12 +22,30 @@ public class WheelOfFortuneSubsystem implements Subsystem, UpdateManager.Updatab
     private final double BLUE_MIN = Constants.WHEEL_OF_FORTUNE_BLUE_HUE - (Constants.WHEEL_OF_FORTUNE_BLUE_HUE - Constants.WHEEL_OF_FORTUNE_GREEN_HUE) / 2;
     private final double BLUE_MAX = Constants.WHEEL_OF_FORTUNE_BLUE_HUE + (210 - Constants.WHEEL_OF_FORTUNE_BLUE_HUE) / 2;
 
+    private static final double SPINNER_POSITION_COEFFICIENT = 0.0;
+    private static final double SPINNER_INTEGRAL_COEFFICIENT = 0.0;
+    private static final double SPINNER_DERIVATIVE_COEFFICIENT = 0.0;
+    private static final double SPINNER_COEFFICIENT = 0.0;
+
     private final I2C.Port i2cPort = I2C.Port.kOnboard;
     private final ColorSensorV3 COLOR_SENSOR = new ColorSensorV3(i2cPort);
 
     private TalonSRX wheelSpinnerMotor = new TalonSRX(Constants.WHEEL_OF_FORTUNE_MOTOR_PORT);
 
     private DetectedColor detectedColor;
+
+    public WheelOfFortuneSubsystem(){
+        super();
+
+        TalonSRXConfiguration spinnerConfig = new TalonSRXConfiguration();
+        spinnerConfig.slot0.kP = SPINNER_POSITION_COEFFICIENT;
+        spinnerConfig.slot0.kI = SPINNER_INTEGRAL_COEFFICIENT;
+        spinnerConfig.slot0.kD = SPINNER_DERIVATIVE_COEFFICIENT;
+        spinnerConfig.primaryPID.selectedFeedbackCoefficient = SPINNER_COEFFICIENT;
+        spinnerConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Absolute;
+
+        wheelSpinnerMotor.configAllSettings(spinnerConfig);
+    }
 
     @Override
     public void periodic(){
