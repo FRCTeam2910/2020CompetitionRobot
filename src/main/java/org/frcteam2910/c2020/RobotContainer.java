bb89b7a468491fa74390e2c409671475f96d8f24
Tsumiki <cppacaba@gmail.com>
2020-01-23 20:39:09 -0800

    (Possibly) correctly working implementation

--- src/main/java/org/frcteam2910/c2020/RobotContainer.java
+++ src/main/java/org/frcteam2910/c2020/RobotContainer.java
@@ -1,7 +1,6 @@
 package org.frcteam2910.c2020;
 
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import org.frcteam2910.c2020.commands.AutoRetractCommand;
 import org.frcteam2910.c2020.commands.DriveCommand;
 import org.frcteam2910.c2020.commands.IntakeCommand;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -29,7 +28,6 @@ public class RobotContainer {
         CommandScheduler.getInstance().setDefaultCommand(drivetrainSubsystem, new DriveCommand(drivetrainSubsystem, getDriveForwardAxis(), getDriveStrafeAxis(), getDriveRotationAxis()));
         CommandScheduler.getInstance().registerSubsystem(feederSubsystem);
         CommandScheduler.getInstance().registerSubsystem(wheelOfFortuneSubsystem);
-        CommandScheduler.getInstance().setDefaultCommand(intakeSubsystem, new AutoRetractCommand(intakeSubsystem, feederSubsystem));
         CommandScheduler.getInstance().registerSubsystem(climberSubsystem);
 
         configureButtonBindings();
@@ -39,7 +37,7 @@ public class RobotContainer {
         primaryController.getBackButton().whenPressed(
                 () -> drivetrainSubsystem.resetGyroAngle(Rotation2.ZERO)
         );
-        primaryController.getLeftBumperButton().whileHeld(new IntakeCommand(intakeSubsystem, 0.5));
+        primaryController.getLeftBumperButton().whileHeld(new IntakeCommand(intakeSubsystem, feederSubsystem, 0.5));
 
         secondaryController.getXButton().whenPressed(new DeployClimberCommand(climberSubsystem));
         secondaryController.getYButton().whenPressed(new ConditionalCommand(
