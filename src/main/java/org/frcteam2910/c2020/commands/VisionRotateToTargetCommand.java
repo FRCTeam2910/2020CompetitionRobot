4bc2e1ddc61fd513f1d34fe19ec04178d9af75ed
Bryce Howard <howardabryce@gmail.com>
2020-01-25 15:21:14 -0800

    Fixes Accessibility Problems
    Co-authored-by: Vlad <vladimirkupryukhin@gmail.com>

--- src/main/java/org/frcteam2910/c2020/commands/VisionRotateToTargetCommand.java
+++ src/main/java/org/frcteam2910/c2020/commands/VisionRotateToTargetCommand.java
@@ -1,7 +1,5 @@
 package org.frcteam2910.c2020.commands;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -11,25 +9,25 @@ import org.frcteam2910.common.control.PidController;
 import org.frcteam2910.common.math.Vector2;
 import org.frcteam2910.common.robot.drivers.Limelight;
 
-public class VisionCommand extends CommandBase {
-    private DrivetrainSubsystem drivetrain;
-    private final PidConstants PID_CONSTANTS = new PidConstants(0.0, 0.0, 0.0);
-    private final String limelightName = "limelight";
+public class VisionRotateToTargetCommand extends CommandBase {
+    private final DrivetrainSubsystem drivetrain;
+    private static final PidConstants PID_CONSTANTS = new PidConstants(0.0, 0.0, 0.0);
+    private static final String limelightName = "LIMELIGHT";
     private PidController controller = new PidController(PID_CONSTANTS);
     private double lastTime = 0.0;
 
-    private final Limelight limelight = new Limelight(NetworkTableInstance.getDefault().getTable(limelightName));
+    private static final Limelight limelight = new Limelight(NetworkTableInstance.getDefault().getTable(limelightName));
 
-    public VisionCommand(DrivetrainSubsystem drivetrain) {
+    public VisionRotateToTargetCommand(DrivetrainSubsystem drivetrain) {
         this.drivetrain = drivetrain;
+
+        addRequirements(drivetrain);
     }
 
     @Override
     public void initialize() {
         lastTime = Timer.getFPGATimestamp();
         limelight.setCamMode(Limelight.CamMode.VISION);
-        controller.setInputRange(Math.toRadians(-27), Math.toRadians(27));
-        controller.setOutputRange(-1.0, 1.0);
     }
 
     @Override
