4b6139cd518ad5be32233192e8c2b8de97b7bb1d
Bryce Howard <howardabryce@gmail.com>
2020-01-25 12:17:18 -0800

    Add Basic Vision Command

---
+++ src/main/java/org/frcteam2910/c2020/commands/VisionCommand.java
@@ -0,0 +1,53 @@
+package org.frcteam2910.c2020.commands;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.frcteam2910.c2020.subsystems.DrivetrainSubsystem;
+import org.frcteam2910.common.control.PidConstants;
+import org.frcteam2910.common.control.PidController;
+import org.frcteam2910.common.math.Vector2;
+import org.frcteam2910.common.robot.drivers.Limelight;
+
+public class VisionCommand extends CommandBase {
+    private DrivetrainSubsystem drivetrain;
+    private final PidConstants PID_CONSTANTS = new PidConstants(0.0, 0.0, 0.0);
+    private final String limelightName = "limelight";
+    private PidController controller = new PidController(PID_CONSTANTS);
+    private double lastTime = 0.0;
+
+    private final Limelight limelight = new Limelight(NetworkTableInstance.getDefault().getTable(limelightName));
+
+    public VisionCommand(DrivetrainSubsystem drivetrain) {
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void initialize() {
+        lastTime = Timer.getFPGATimestamp();
+        limelight.setCamMode(Limelight.CamMode.VISION);
+        controller.setInputRange(Math.toRadians(-27), Math.toRadians(27));
+        controller.setOutputRange(-1.0, 1.0);
+    }
+
+    @Override
+    public void execute() {
+        double time = Timer.getFPGATimestamp();
+        double dt = time - lastTime;
+
+
+        if(limelight.hasTarget()) {
+            double angle = limelight.getTargetPosition().x;
+            controller.setSetpoint(angle);
+            double rotationalVelocity = controller.calculate(time, dt);
+            drivetrain.drive(Vector2.ZERO, rotationalVelocity, false);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return limelight.getTargetPosition().x == 0;
+    }
+}
